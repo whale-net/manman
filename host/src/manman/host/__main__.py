97c07ff7ff5238b76d13e87255eac063c3a84bfe
qmastermoo <cpsaicontact@gmail.com>
2024-04-24 21:24:40 -0400

    these have been here a while not really sure

--- host/src/manman/host/__main__.py
+++ host/src/manman/host/__main__.py
@@ -1,8 +1,6 @@
 
 if __name__ == '__main__':
     print('hello world, it is I, host')
-    import black
-    print('black imported')
 
     from manman.test_shared.sample import Sample
     print(Sample.value)
